d6eb4cd31238683f3f80781d1b5030eb954d1dd0
Carolina Ventura <carolventura1996@gmail.com>
2021-04-23 21:50:28 +0100

    previous and next button

--- podcastrnext/src/components/Player/index.tsx
+++ podcastrnext/src/components/Player/index.tsx
@@ -15,7 +15,9 @@ export function Player() {
     currentEpisodeIndex, 
     isPlaying, 
     togglePlay, 
-    setPlayingState
+    setPlayingState,
+    playNext,
+    playPrevious,
   } = useContext(PlayerContext)
 
   useEffect(() => {
@@ -88,7 +90,9 @@ export function Player() {
           <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar" />
           </button>
-          <button type="button" disabled={!episode}>
+          <button type="button"
+          onClick={playPrevious}
+          disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
           <button type="button"
@@ -102,7 +106,9 @@ export function Player() {
             } 
            
           </button>
-          <button type="button" disabled={!episode}>
+          <button type="button"
+          onClick={playNext}
+          disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar próxima" />
           </button>
           <button type="button" disabled={!episode}>
